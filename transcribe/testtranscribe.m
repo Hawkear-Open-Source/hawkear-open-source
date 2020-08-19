14b8b4ee5c82179c39a2fc3c8cac494592855f90
ianhawkdev <ian.mccallion@gmail.com>
2020-08-20 00:27:25 +0100

    Slight improvement in the testcase and a fix to my temporary bodge code

--- transcribe/testtranscribe.m
+++ transcribe/testtranscribe.m
@@ -1,8 +1,15 @@
 fqdntest = fullfile(getenv('APPDATA'), 'HOS','testdata');
+fqfnLowndes = fullfile(fqdntest, 'Thunston.20200202-0000.s8t8.1.txt');
+[a,b]=unlink (fqfnLowndes)
 transcribe(fullfile(fqdntest, 'Thunston.20200202-0000.s8t8.1.wav'), ...
            fullfile(fqdntest, 'Thunston.train.bin'), ...
 		   fullfile(fqdntest,'Thunston.20200202-0000.s8t8.1.txt'), ...
-		   '-fl:320', '-qt:0');
-		   disp('Tesp completed. This file should have been created:')
-		   system (['dir ', fqdntest, '\Thunston.20200202-0000.s8t8.1.txt'])
+		  % '-fl:320',...
+		   '-qt:2');
+		   if exist(fqfnLowndes,'file')
+		       disp('Test successful')
+    		   system (['dir ', fqdntest, '\Thunston.20200202-0000.s8t8.1.txt'])
+		   else
+		       disp('Test failed')
+		   end
 % Now check the result		   
